05da28f2964390c8b34f34b0f361ff20f0ae3946
lijinming11 <96190888+lijinming11@users.noreply.github.com>
2021-12-18 22:08:58 +0800

    pull commit

--- src/main/com/lijinmin/jdk8/GitTest.java
+++ src/main/com/lijinmin/jdk8/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master!");
         System.out.println("test master");
         System.out.println("push master");
+        System.out.println("pull master");
     }
 }
