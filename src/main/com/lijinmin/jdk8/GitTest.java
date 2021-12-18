e36b2f97f505c734f632f0a61dbbaa47e15d3ef0
lijinmin <lijinmin11@163.com>
2021-12-18 21:05:25 +0800

    master-test2

--- src/main/com/lijinmin/jdk8/GitTest.java
+++ src/main/com/lijinmin/jdk8/GitTest.java
@@ -6,6 +6,6 @@ public class GitTest {
         System.out.println("test2");
         System.out.println("test3");
         System.out.println("test4");
-        System.out.println("test5");
+        System.out.println("test master");
     }
 }
